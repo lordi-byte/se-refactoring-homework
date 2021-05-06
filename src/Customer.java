fd5380787fc9d1c94b5d5d64e376b75de24c7da7
Ian Helmrich <ian.helmrich@gmail.com>
2021-05-06 17:20:33 +0200

    add a htmlStatement method

--- src/Customer.java
+++ src/Customer.java
@@ -31,6 +31,21 @@ class Customer {
         return result;
     }
 
+    public String htmlStatement() {
+        Enumeration rentals = this.rentals.elements();
+        String result = "<H1>Rentals for <EM>" + getName() + "</EM></ H1><P>\n";
+        while (rentals.hasMoreElements()) {
+            Rental each = (Rental) rentals.nextElement();
+            //show figures for each rental
+            result += each.getMovie().getTitle()+ ": " + String.valueOf(each.getCharge()) + "<BR>\n";
+        }
+        //add footer lines
+        result += "<P>You owe <EM>" + String.valueOf(getTotalCharge()) + "</EM><P>\n";
+        result += "On this rental you earned <EM>" + String.valueOf(getTotalFrequentRenterPoints()) + "</EM> frequent renter points<P>";
+        return result;
+    }
+
+
     private double getTotalCharge() {
         double result = 0;
         Enumeration enum_rentals = rentals.elements();
@@ -41,8 +56,7 @@ class Customer {
         return result;
     }
 
-    private int
-    getTotalFrequentRenterPoints() {
+    private int getTotalFrequentRenterPoints() {
         int result = 0;
         Enumeration rentals = this.rentals.elements();
         while (rentals.hasMoreElements()) {
